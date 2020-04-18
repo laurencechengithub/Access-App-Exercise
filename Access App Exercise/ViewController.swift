2a84a13b2e1d8bccfdb36c3cd10ad6c5b0540763
LaurenceMini <laurence.email@gmail.com>
2020-04-18 23:58:36 +0800

    Initial Commit

---
+++ Access App Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Access App Exercise
+//
+//  Created by LaurenceMini on 2020/4/18.
+//  Copyright © 2020 LaurenceMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
