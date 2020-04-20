5fee56d54c5d5d01a55b632d7430cf64bd0348fa
LaurenceMini <laurence.email@gmail.com>
2020-04-20 15:32:31 +0800

    [delete] alamofire from networkManager

--- Access App Exercise/NetWorkManager.swift
+++ Access App Exercise/NetWorkManager.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import Alamofire
 import SwiftyJSON
 
 class NetWorkManager {
@@ -27,28 +26,6 @@ class NetWorkManager {
             }
         }
     }
-    
-    
-//    private func baseRequest(url:String,
-//                     method:HTTPMethod = .get,
-//                     parameters:Parameters? = nil,
-//                     encoding:ParameterEncoding = URLEncoding.default,
-//                     headers: HTTPHeaders? = nil,
-//                     completionHandler:@escaping (_ responseJSON: JSON?) -> ()) -> DataRequest {
-//
-//        return AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers).responseJSON { [weak self] (responseData) in
-//
-//            switch responseData.result {
-//            case .success(let value):
-//                let jsonData = JSON(value)
-//                completionHandler(jsonData)
-//            case .failure(let error):
-//                print(" baseRequest error : \(error)")
-//                completionHandler(nil)
-//            }
-//        }
-//
-//    }
     
     
     let defaultSession = URLSession(configuration: .default)
